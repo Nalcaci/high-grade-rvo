d9fd467345b8b17e480adde2dbf8e7233f238912
Dwarfilicious <zoeteman1@gmail.com>
2024-12-19 14:25:59 +0100

    Fixed a bunch of stuff

--- Assets/Scripts/AgentSpawner.cs
+++ Assets/Scripts/AgentSpawner.cs
@@ -5,31 +5,32 @@ public class AgentSpawner:MonoBehaviour
 {
     public GameObject agentPrefab;
     public GameObject target;
-    public Color32 colour;
+    public Color32 color;
     public float timer = 0.5f;
     public int spawnAmount = 500;
-    public static void SpawnAgent(GameObject agentPrefab, Vector3 position, Quaternion rotation, Vector3 target, Color32 color)
+    private int spawnCount = 0;
+
+    void SpawnAgent(GameObject agentPrefab, Vector3 position, Quaternion rotation, Vector3 target, Color32 color)
     {
         GameObject agent = Object.Instantiate(agentPrefab, position+new Vector3(Random.Range(-10,10),0, Random.Range(-10, 10)), rotation);
         agent.name = "Agent";
-        AddingWaypoint agentThing = agent.GetComponent<AddingWaypoint>();
-        agentThing.goal = target;
+        agent.GetComponent<AddingWaypoint>().target = target;
         Renderer render = agent.GetComponent<Renderer>();
         render.material.color = color;
     }
+
     void Start()
     {
-        SpawnAgent(agentPrefab, this.transform.position, Quaternion.identity,target.transform.position, colour);
         StartCoroutine(Spawning());
     }
 
     public IEnumerator Spawning()
     {
         yield return new WaitForSeconds(timer);
-        if (spawnAmount >= 0)
+        if (spawnCount < spawnAmount)
         {
-            SpawnAgent(agentPrefab, this.transform.position, Quaternion.identity, target.transform.position, colour);
-            spawnAmount -= 1;
+            SpawnAgent(agentPrefab, this.transform.position, Quaternion.identity, target.transform.position, color);
+            spawnCount += 1;
             StartCoroutine(Spawning());
         }
     }
