d0a09437e123c3da6507caee931898a6727ef6a9
Jordi van der Lem <43111967+jvdlem@users.noreply.github.com>
2025-01-19 23:40:45 +0100

    Added extra smoothing
    Fixed drawing added smoothness

--- Assets/Scripts/AddingWaypoint.cs
+++ Assets/Scripts/AddingWaypoint.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using Unity.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class AddingWaypoint : MonoBehaviour
 {
@@ -17,6 +18,9 @@ public class AddingWaypoint : MonoBehaviour
     public float smoothingLength = 1;
     [SerializeField]
     private int smoothingSections = 10;
+    [SerializeField]
+    [Range(-1, 1)]
+    private float smoothingFactor = 0;
 
     public Vector3[] pathLocations = new Vector3[0];
     [SerializeField]
@@ -34,7 +38,7 @@ public class AddingWaypoint : MonoBehaviour
     private float lerpTime = 0;
     [SerializeField]
     private Vector3 movementVector;
-
+    private Vector3 infinityVector = new Vector3(float.PositiveInfinity, float.PositiveInfinity, float.PositiveInfinity);
     private bool debug = false;
 
     void Update()
@@ -170,22 +174,119 @@ public class AddingWaypoint : MonoBehaviour
                 index++;
             }
         }
-
+        pathLocations = PostProcessPath(curves, pathLocations);
         return pathLocations;
+    }
+
+    private Vector3[] PostProcessPath(BezierCurve[] Curves, Vector3[] Path)
+    {
+        Vector3[] path = RemoveOversmoothing(Curves, Path);
+
+        path = RemoveTooClosePoints(path);
+
+        path = SamplePathPositions(path);
+
+        return path;
+    }
+
+    private Vector3[] RemoveOversmoothing(BezierCurve[] Curves, Vector3[] Path)
+    {
+        if (Path.Length <= 2)
+        {
+            return Path;
+        }
+
+        int index = 1;
+        int lastIndex = 0;
+        for (int i = 0; i < Curves.Length; i++)
+        {
+            Vector3 targetDirection = (Curves[i].EndPosition - Curves[i].StartPosition).normalized;
+
+            for (int j = 0; j < smoothingSections - 1; j++)
+            {
+                Vector3 segmentDirection = (Path[index] - Path[lastIndex]).normalized;
+                float dot = Vector3.Dot(targetDirection, segmentDirection);
+                Debug.Log($"Target Direction: {targetDirection}. segment direction: {segmentDirection} = dot {dot} with index {index} & lastIndex {lastIndex}");
+                if (dot <= smoothingFactor)
+                {
+                    Path[index] = infinityVector;
+                }
+                else
+                {
+                    lastIndex = index;
+                }
+
+                index++;
+            }
+
+            index++;
+        }
+
+        Path[Path.Length - 1] = Curves[Curves.Length - 1].EndPosition;
+
+        Vector3[] TrimmedPath = Path.Except(new Vector3[] { infinityVector }).ToArray();
+
+        Debug.Log($"Original Smoothed Path: {Path.Length}. Trimmed Path: {TrimmedPath.Length}");
+
+        return TrimmedPath;
+    }
+
+    private Vector3[] SamplePathPositions(Vector3[] Path)
+    {
+        for (int i = 0; i < Path.Length; i++)
+        {
+            if (NavMesh.SamplePosition(Path[i], out NavMeshHit hit, thisAgent.radius * 1.5f, thisAgent.areaMask))
+            {
+                Path[i] = hit.position;
+            }
+            else
+            {
+                Debug.LogWarning($"No NavMesh point close to {Path[i]}. Check your smoothing settings!");
+                Path[i] = infinityVector;
+            }
+        }
+
+        return Path.Except(new Vector3[] { infinityVector }).ToArray();
+    }
+
+    private Vector3[] RemoveTooClosePoints(Vector3[] Path)
+    {
+        if (Path.Length <= 2)
+        {
+            return Path;
+        }
+
+        int lastIndex = 0;
+        int index = 1;
+        for (int i = 0; i < Path.Length - 1; i++)
+        {
+            if (Vector3.Distance(Path[index], Path[lastIndex]) <= thisAgent.radius)
+            {
+                Path[index] = infinityVector;
+            }
+            else
+            {
+                lastIndex = index;
+            }
+            index++;
+        }
+
+        return Path.Except(new Vector3[] { infinityVector }).ToArray();
     }
 
     void DrawPath()
     {
-        int numLocations = pathLocations.Length;
-        pathRenderer.positionCount = numLocations;
+        if (pathIndex >= pathLocations.Length) return;
+
+        int remainingPoints = pathLocations.Length - pathIndex;
+        pathRenderer.positionCount = remainingPoints + 1; // Include current position
         pathRenderer.SetPosition(0, transform.position);
 
-        if(numLocations < 2) { return; }
-
-        for(int i = 1; i < numLocations; i++)
+        for (int i = 0; i < remainingPoints; i++)
         {
-            Vector3 pointPosition = new Vector3(pathLocations[i].x, pathLocations[i].y, pathLocations[i].z);
-            pathRenderer.SetPosition(i, pointPosition);
+            Vector3 pointPosition = pathLocations[pathIndex + i];
+            pathRenderer.SetPosition(i + 1, pointPosition);
         }
     }
+
 }
